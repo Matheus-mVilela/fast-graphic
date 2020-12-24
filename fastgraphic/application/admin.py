d4bfafb2c8a575db286df4c2df4c5522424bc8cd
Matheus_Vilela <matheus_vilela@icloud.com>
2020-12-23 21:46:42 -0300

    create and register class for admin

--- fastgraphic/application/admin.py
+++ fastgraphic/application/admin.py
@@ -2,7 +2,28 @@ from django.contrib import admin
 from .models import Employee, Product, SaleProduct, Sale
 
 
-admin.site.register(Employee)
-admin.site.register(Product)
-admin.site.register(SaleProduct)
-admin.site.register(Sale)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+class SaleProductInline(admin.StackedInline):
+    pass
+
+
+class SaleProductAdmin(admin.ModelAdmin):
+    pass
+
+
+class SaleAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(SaleProduct, SaleProductAdmin)
+admin.site.register(Sale, SaleAdmin)
+
